7e31c33f5000efee8971b9fbfdf6d29f1b86b800
IamNator <natverior1@gmail.com>
2021-04-17 07:58:52 +0100

    first commit

---
+++ http_error.go
@@ -0,0 +1,176 @@
+package httperror
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+//Error ...
+type _error struct {
+	Code    int         `json:"code"`              //e.g 201, 200, 401
+	Status  bool        `json:"status"`            //e.g false, true
+	Name    string      `json:"name,omitempty"`    //name of the error
+	Err     error       `json:"err,omitempty"`     //standard go error
+	Message string      `json:"message,omitempty"` // any message for the error
+	Error   interface{} `json:"error,omitempty"`   //custom error
+}
+
+//New returns a new object
+//
+// status is false by default
+func New(err error) _error {
+
+	//Check for empty(nil) error
+	var msg string
+	if err != nil {
+		msg = err.Error()
+	} else {
+		msg = ""
+	}
+
+	return _error{Status: false, Err: err, Message: msg, Error: msg}
+}
+
+/************************************SET FIELDS*********************************/
+
+//SetStatus sets status field
+func (e *_error) SetStatus(status bool) {
+	e.Status = status
+}
+
+//SetCode sets code field
+func (e *_error) SetCode(code int) {
+	e.Code = code
+}
+
+//SetName sets name field
+func (e *_error) SetName(name string) {
+	e.Name = name
+}
+
+//SetErr sets err field
+func (e *_error) SetErr(er error) {
+	e.Err = er
+}
+
+//SetMessage sets message field
+func (e *_error) SetMessage(message string) {
+	e.Message = message
+}
+
+//SetError sets error field
+func (e *_error) SetError(er interface{}) {
+	e.Error = er
+}
+
+/**********************************GET FIELDS***************************************/
+
+//GetStatus gets status field
+func (e _error) GetStatus() bool {
+	return e.Status
+}
+
+//GetCode gets code field
+func (e _error) GetCode() int {
+	return e.Code
+}
+
+//GetName gets name field
+func (e _error) GetName() string {
+	return e.Name
+}
+
+//GetErr gets err field
+func (e _error) GetErr() error {
+	return e.Err
+}
+
+//GetMessage sets message field
+func (e _error) GetMessage() string {
+	return e.Message
+}
+
+//GetError gets error field
+func (e _error) GetError() interface{} {
+	return e.Error
+}
+
+/******************************** HTTP REPLY***********************************/
+func (e _error) ReplyInternalServerError(w http.ResponseWriter) {
+	e.Code = http.StatusInternalServerError
+	e.Reply(w)
+}
+
+func (e _error) ReplyBadRequest(w http.ResponseWriter) {
+	e.Code = http.StatusBadRequest
+	e.Reply(w)
+}
+
+func (e _error) Reply(w http.ResponseWriter) {
+
+	w.WriteHeader(e.Code) //Write http code of error
+
+	if er := json.NewEncoder(w).Encode(e.web()); er != nil { //encode error message
+		http.Error(w, er.Error(), http.StatusInternalServerError)
+	}
+}
+
+//Removes standard go error field
+func (e _error) web() interface{} {
+
+	return struct {
+		Code    int         `json:"code"`              //e.g 201, 200, 401
+		Status  bool        `json:"status"`            //e.g false, true
+		Name    string      `json:"name,omitempty"`    //name of the error
+		Message string      `json:"message,omitempty"` // any message for the error
+		Error   interface{} `json:"error,omitempty"`   //custom error
+	}{
+		Code:    e.Code,
+		Status:  e.Status,
+		Name:    e.Name,
+		Message: e.Message,
+		Error:   e.Error,
+	}
+}
+
+/**************** OTHERS ************************/
+
+//Returns the error in string format
+func (e _error) String() string {
+	var status string
+	if e.Status {
+		status = "true"
+	} else {
+		status = "false"
+	}
+	return "code: " + strconv.Itoa(e.Code) + "\nstatus: " + status + "\nerror: " + e.Err.Error()
+}
+
+//Read reads the next len(p) bytes from the buffer
+//or until the buffer is drained.
+//The return value n is the number of bytes read. If the buffer has no data to return,
+// err is io.EOF (unless len(p) is zero); otherwise it is nil.
+func (e _error) Read(p []byte) (n int, err error) {
+	b, err := json.Marshal(e)
+
+	if err == nil {
+		buf := bytes.NewBuffer(b)
+		n, err = buf.Read(p)
+	}
+
+	return
+}
+
+//writes to io.writer interface
+func (e _error) ReadToWriter(w io.Writer) (n int, err error) {
+	b, err := json.Marshal(e)
+
+	if err == nil {
+		n, err = w.Write(b)
+	}
+
+	return
+}
